da10cdbd1718d75447ca27d6bc1c4703c85708dd
gauravm-bit <gauravwrk01@gmail.com>
2020-03-27 12:46:34 +0530

    ADDED:Testcase 1.1 which returns sad

--- test/java/TestCases.java
+++ test/java/TestCases.java
@@ -2,5 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCases {
+    AnalyzerFactory analyzerFactory = new AnalyzerFactory();
 
+    @Test
+    public void givenMessage_whenSad_shouldReturn_sad(){
+        String emotion= analyzerFactory.analyzeMood("I am sad");
+        Assert.assertEquals("Sad",emotion);
+    }
 }
