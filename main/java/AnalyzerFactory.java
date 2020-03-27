353a9142c7fc91754eef71b840f7cf2404d2ce3c
gauravm-bit <gauravwrk01@gmail.com>
2020-03-27 11:59:22 +0530

    ADDED:Object creation with mood analyser function which returns mood.

--- main/java/AnalyzerFactory.java
+++ main/java/AnalyzerFactory.java
@@ -1,5 +1,17 @@
 public class AnalyzerFactory {
     public static void main(String[] args) {
+        AnalyzerFactory MoodAnalyser = new AnalyzerFactory();
+        MoodAnalyser.analyzeMood("sad");
         System.out.println("Welcome to MoodAnalyzer");
+
     }
+    public String analyzeMood(String emotion){
+        if (emotion.contains("sad"))
+            return "Sad";
+        else
+            return "Happy";
+    }
+
+
+
 }
