b0003af6cb49105e696bffa9d1697f0f49a0022f
gauravm-bit <gauravwrk01@gmail.com>
2020-03-30 15:22:57 +0530

    REFACTORED: passed values through test cases and commenting added.

--- main/java/MoodAnalysisFactory.java
+++ main/java/MoodAnalysisFactory.java
@@ -2,16 +2,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalysisFactory {
-    public static MoodAnalyser createMoodAnalyserObject() throws MoodAnalysisException{
+    public static MoodAnalyser createMoodAnalyserObject(Constructor constructor) throws MoodAnalysisException{
         try {
-            Class moodAnalyserClass = Class.forName("MoodAnalyser");
-            Constructor moodConstructor = moodAnalyserClass.getConstructor();
-            Object moodObject = moodConstructor.newInstance();
-            return (MoodAnalyser) moodObject;
-        } catch (ClassNotFoundException e) {
-            throw new MoodAnalysisException("class not found",MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS);
-        } catch (NoSuchMethodException e) {
-            throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD);
+            return (MoodAnalyser) constructor.newInstance();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
@@ -21,4 +14,16 @@ public class MoodAnalysisFactory {
         }
         return null;
     }
+
+    public static Constructor getConstructor(String className,Class<?>... param) throws MoodAnalysisException {
+        try {
+            Class<?> moodClass = Class.forName(className);
+            return moodClass.getConstructor(param);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException("class not found",MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD);
+        }
+    }
+
 }
--- test/java/TestCases.java
+++ test/java/TestCases.java
@@ -1,27 +1,33 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+
 public class TestCases {
     MoodAnalyser moodAnalyser;
 
+    //TC 1.1
     @Test
     public void givenMessage_whenSad_shouldReturn_sad() throws MoodAnalysisException {
         moodAnalyser = new MoodAnalyser("I am in sad mood");
         Assert.assertEquals("Sad", moodAnalyser.analyzeMood());
     }
 
+    //TC 1.2
     @Test
     public void givenMessage_whenAnyMood_shouldReturn_Happy() throws MoodAnalysisException {
-        moodAnalyser = new MoodAnalyser("I am in any mood");
+        moodAnalyser = new MoodAnalyser("I am in happy mood");
         Assert.assertEquals("Happy", moodAnalyser.analyzeMood());
     }
 
+    //TC 2.1
     @Test
     public void givenMessage_whenNull_shouldReturn_Happy() throws MoodAnalysisException {
         moodAnalyser = new MoodAnalyser(null);
         Assert.assertEquals("Happy", moodAnalyser.analyzeMood());
     }
 
+    //TC 3.1
     @Test
     public void givenMessage_WhenNull_ThenShouldReturnNullException() {
         try {
@@ -32,6 +38,7 @@ public class TestCases {
         }
     }
 
+    //TC 3.2
     @Test
     public void givenMessage_WhenEmpty_ThenShouldReturnEmptyException() {
         try {
@@ -42,27 +49,31 @@ public class TestCases {
         }
     }
 
+    //TC 4.1
     @Test
     public void givenObject_WhenEquals_ThenReturnTrue() throws MoodAnalysisException{
         moodAnalyser=new MoodAnalyser();
-        MoodAnalyser moodAnalyserObject = MoodAnalysisFactory.createMoodAnalyserObject();
+        Constructor constructor=MoodAnalysisFactory.getConstructor("MoodAnalyser");
+        MoodAnalyser moodAnalyserObject = MoodAnalysisFactory.createMoodAnalyserObject(constructor);
         boolean result = moodAnalyser.equals(moodAnalyserObject);
         Assert.assertTrue("true",result);
     }
 
+    // 4.2
     @Test
     public void givenClass_WhenWrong_ThenShouldReturnClassNotFound() {
         try {
-            MoodAnalysisFactory.createMoodAnalyserObject();
+            MoodAnalysisFactory.getConstructor("Measdsda");
         } catch (MoodAnalysisException e) {
             Assert.assertEquals(MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS,e.userDefinedType);
         }
     }
 
+    // 4.3
     @Test
     public void givenMethod_WhenWrong_ThenShouldReturnNoSuchMethod() {
         try {
-            MoodAnalysisFactory.createMoodAnalyserObject();
+            MoodAnalysisFactory.getConstructor("MoodAnalyser",Integer.class);
         } catch (MoodAnalysisException e) {
             Assert.assertEquals(MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD,e.userDefinedType);
         }
