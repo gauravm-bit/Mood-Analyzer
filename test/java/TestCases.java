3a52eb03731259b7d4e9b612e3154d44bec6d3ba
gauravm-bit <gauravwrk01@gmail.com>
2020-03-30 14:24:32 +0530

    MODIFIED: commenting added in code

--- test/java/TestCases.java
+++ test/java/TestCases.java
@@ -3,15 +3,17 @@ import org.junit.Test;
 
 public class TestCases {
 
+    //TC1.1
     @Test
     public void givenMessage_whenSad_shouldReturn_sad(){
         MoodAnalyser moodAnalyser=new MoodAnalyser("I am in sad mood");
         Assert.assertEquals("Sad",moodAnalyser.analyzeMood());
     }
 
+    //TC1.2
     @Test
     public void givenMessage_whenAnyMood_shouldReturn_Happy(){
-        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in any mood");
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in happy mood");
         Assert.assertEquals("Happy",moodAnalyser.analyzeMood());
     }
 
