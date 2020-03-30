c7876be024605f15a9c2b40ed981f0f2f39fbccc
gauravm-bit <gauravwrk01@gmail.com>
2020-03-30 14:35:33 +0530

    MODIFIED: commenting added in code

--- test/java/TestCases.java
+++ test/java/TestCases.java
@@ -3,24 +3,28 @@ import org.junit.Test;
 
 public class TestCases {
 
+    //TC 1.1
     @Test
     public void givenMessage_whenSad_shouldReturn_sad() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
         Assert.assertEquals("Sad", moodAnalyser.analyzeMood());
     }
 
+    //TC 1.2
     @Test
     public void givenMessage_whenAnyMood_shouldReturn_Happy() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
         Assert.assertEquals("Happy", moodAnalyser.analyzeMood());
     }
 
+    //TC 2.1
     @Test
     public void givenMessage_whenNull_shouldReturn_Happy() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         Assert.assertEquals("Happy", moodAnalyser.analyzeMood());
     }
 
+    //TC 3.1
     @Test
     public void givenMessage_WhenNull_ThenShouldReturnNullException() {
         try {
@@ -31,6 +35,7 @@ public class TestCases {
         }
     }
 
+    //TC 3.2
     @Test
     public void givenMessage_WhenEmpty_ThenShouldReturnEmptyException() {
         try {
